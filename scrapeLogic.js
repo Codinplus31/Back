22bdc8739a95f4c349ab69c50513c76d3d058ffd
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-02-15 11:26:20 +0100

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,29 +17,29 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://watchoutmovies.vercel.app/");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+   // await page.type(".search-box__input", "automate beyond recorder");
 
     // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
+    const searchResultSelector = ".container";
     await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    //await page.click(searchResultSelector);
 
     // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
+    //const textSelector = await page.waitForSelector(
       "text/Customize and automate"
     );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    //const fullTitle = await searchResultSelector.evaluate((el) => el.textContent);
 
     // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+   // const logStatement = ${fullTitle}`;
+    console.log(searchResultSelector);
+    res.send(searchResultSelector);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
