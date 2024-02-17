d7dcd493497c529638edea67f42424c3b95b4527
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-02-17 16:35:33 +0100

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -40,7 +40,8 @@ const Tag = async (res) => {
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
-
+await page.screenshot({ path: 'screenshot.png' });
+  
     // Type into search box
    // await page.type(".search-box__input", "automate beyond recorder");
 
