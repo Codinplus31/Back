e740acb2950928b443ef97200b3dd5a906f661ad
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-02-17 16:16:07 +0100

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -85,14 +85,14 @@ const Explore = async (res) => {
   });
   try {
     const page = await browser.newPage();
-await page.goto("https://www.aliexpress.com/?spm=a2g0n.category.header.2.6d4bzxXLzxXLUC");
+await page.goto("https://campaign.aliexpress.com/wow/gcp-plus/ae/tupr?spm=a2g0n.home.choice.1.3dc876dbtcDRdU&_immersiveMode=true&wx_navbar_hidden=true&wx_navbar_transparent=true&ignoreNavigationBar=true&wx_statusbar_hidden=true&wh_weex=true&wh_pid=300000557%2FChoiceFirstN&browser_id=a37dc6672b1046049dccd0d742a4a070&aff_trace_key=null&aff_platform=msite&m_page_id=kwtwhonnjmcawzzl18db79218b01ead5af17790669&gclid=");
    //await page.screenshot({ path: 'screenshot.png' });
       //https://www.aliexpress.com/?spm=a2g0n.category.header.2.6d4bzxXLzxXLUC
   
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
-      const imageUrls = await page.$$eval('[class=notranslate]', images => images.map(img => img.innerHTML));
+      const imageUrls = await page.$$eval('[class=_1CCSZ]', images => images.map(img => img.innerHTML));
     
     res.send(imageUrls);
   } catch (e) {
