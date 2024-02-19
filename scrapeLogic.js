75d7aa5481c30d70b3bb38a1c38aa277a9027079
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-02-19 10:06:10 +0100

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -93,9 +93,30 @@ await page.goto("https://www.aliexpress.com/w/wholesale-recommend.html");
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024});
 //page.setDefaultNavigationTimeout(60000);
-      const imageUrls = await page.$$eval('img', images => images.map(img=> img.src));
+     // const imageUrls = await page.$$eval('img', images => images.map(img=> img.src));
+   let selector = "main--card-2NMkE1D";
+      await page.waitForSelector(selector);
+const data = await page.evaluate((selector) => {
+      const e = document.querySelector(selector);
+ let img = e.querySelector(".product-img").src;
+let title = e.querySelector(".manhattan--titleText--WccSjUS").textContent;
+let saleprice = e.querySelector(".manhattan--price-sale--1CCSZfK").textContent;
+let originalprice = e.querySelector(".manhattan--price-original--3QAcCkG").textContent;
+
+      // Your scraping logic goes here
+      // You can access and manipulate the element using standard DOM APIs
+      // Return the extracted data as an object or array
+      return {
+        img: img,
+        title: title,
+        saleprice:saleprice,
+        originalprice:originalprice
+          
+        // Add more properties as needed
+      };
+    }, selector);
     
-    res.send(imageUrls);
+    res.send(data);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
