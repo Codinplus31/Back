f0dd13120b9c07aae03dae2aafb1be6cd11f1ad4
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-02-21 10:16:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 //const { Tag, Explore } = require("./scrapeLogic");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -17,15 +19,15 @@ app.get("/", (req, res) => {
   fetch("https://www.aliexpress.com/w/wholesale-Recommend.html?osf=history&spm=a2g0n.category.header.0")
   .then(response => response.text())
   .then(html => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    let arr = [];
-    const element = doc.querySelectorAll("span")
+      let arr = [];
+   const dom = new JSDOM(html);
+const document = dom.window.document;
+    const element = document.querySelectorAll("span")
       .forEach(e=>{
-      //arr.push(e)
-        res.send(e)
+      arr.push(e)
+        
       });
-
+res.send(arr)
     // Do something with the selected element
     
   });
