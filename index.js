72a2cbf707e317823db1300615aaadf99dce1447
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-02-21 10:02:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const { Tag, Explore } = require("./scrapeLogic");
+//const { Tag, Explore } = require("./scrapeLogic");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/Tag", (req, res) => {
-  Tag(res);
+ // Tag(res);
 });
 
 app.get("/Explore", (req, res) => {
-  Explore(res);
+  //Explore(res);
 });
     
                  
