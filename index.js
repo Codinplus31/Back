273a052d3bd3ca19c63ff7ee1dd974da5aa76608
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-02-26 14:54:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const documents = doms.window.document;
      /*if(f.parentNode.tagName === "span"){
         return f.textContent
      }*/
-    return f.parent.id
+    return f.parentNode.id
     })
 let arr1 = [];
      for(let i = 0; i < cat.length; i++){
