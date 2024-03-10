4bc3ddff359a4bc61f9567542a5c9ff7f6146753
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-03-10 23:48:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,32 @@ const { JSDOM } = jsdom;
 const app = express();
 app.use(cors())
 const PORT = process.env.PORT || 4000;
+app.get('/category',(req,res) =>{
+ fetch("https://www.aliexpress.com/p/calp-plus/index.html?osf=category_navigate_newTab2&queryFrom=kingKong&categoryTab=babies_%26_kids")
+  .then(response => response.text())
+  .then(html => {
+     let arr = [];
+   const dom = new JSDOM(html);
+const document = dom.window.document;
+   //  function Len(l){
+const e = document.querySelectorAll("#card-list")[0]
+  
+   const title = Array.from(e.querySelectorAll(".multi--titleText--nXeOvyr")).map(f=> f.textContent)
+      const img = Array.from(e.querySelectorAll("div.images--imageWindow--1Z-J9gn")).map(f=> f.querySelectorAll(".images--item--3XZa6xf")[0].src);
+      const price = Array.from(e.querySelectorAll(".multi--price-sale--U-S0jtj")).map(f=> f.textContent)
+      // const owner = e.querySelector(".cards--storeLink--XkKUQFS")
+     const link = Array.from(e.querySelectorAll(".multi--container--1UZxxHY")).map(f=> f.href)
+       
+      
 
+     
+ 
+  for(let i = 0; i < title.length; i++){
+arr.push({title: title[i],img: img[i],price: price[i],link: link[i]})
+     }
+ res.json([{"explore":arr}])
+  }})
+});
 app.get("/Tag", (req, res) => {
 
  fetch("https://www.aliexpress.com/w/wholesale-Recommend.html?osf=history&spm=a2g0n.category.header.0")
