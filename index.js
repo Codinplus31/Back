6f0d260307db66ddaad9b8b3d64d4c1cbeb9ad7d
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-03-16 20:15:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ arr.push({title: title[i],img: img[i],price: price[i],link: link[i]})
     
                  
 app.get("/", (req, res) => {
-  fetch("https://www.aliexpress.com/w/wholesale-Recommend.html?osf=history&spm=a2g0n.category.header.0")
+  fetch("https://www.aliexpress.com/w/wholesale-suggest.html")
   .then(response => response.text())
   .then(html => {
       let arr = [];
