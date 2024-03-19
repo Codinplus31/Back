0012314477c02ed9c8f4cab253715196a8e6ceb5
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-03-19 07:06:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ const document = dom.window.document;
 const e = document.querySelectorAll("#card-list")[0]
   
    const title = Array.from(e.querySelectorAll(".multi--titleText--nXeOvyr")).map(f=> f.textContent)
-      const img = Array.from(e.querySelectorAll("div.images--imageWindow--1Z-J9gn")).map(f=> f.querySelector(".images--item--3XZa6xf").src);
+      const img = Array.from(e.querySelectorAll("div.images--imageWindow--1Z-J9gn")).map(f=> f.querySelectorAll(".images--item--3XZa6xf")[0].src);
       const price = Array.from(e.querySelectorAll(".multi--price-sale--U-S0jtj")).map(f=> f.textContent)
       // const owner = e.querySelector(".cards--storeLink--XkKUQFS")
      const link = Array.from(e.querySelectorAll(".multi--container--1UZxxHY")).map(f=> f.href)
