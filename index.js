407a67f6cb57a47d756fc2890cad9b2270c9ff33
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-03-23 17:16:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const document = dom.window.document;
 //   for(let i = 0; i < title.length; i++){
 // arr.push({title: title[i],img: img[i],price: price[i],link: link[i]})
 //      }
- res.json([{"details":{title:title,img: img, price:price}])
+ res.json([{"details":{title:title,img: img, price:price}}])
   })
 });
 app.get('/category',(req,res) =>{
